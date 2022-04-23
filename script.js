f46a67289725d203e48e9d6f6ce813d174b526d7
simpleBoy <simpleBoy@mail.com>
2022-04-24 00:01:54 +0200

    files added

---
+++ script.js
@@ -0,0 +1,18 @@
+// Birinchi uy ishi !!!
+
+// do {
+//     y = +prompt('son kiriting')
+// } while (isNaN(y) || y ==0);
+// y % 2 == 0 ? alert('juft') : alert('toq')
+
+// Ikkinchi uy ishi !!!
+
+// let son = prompt('Son kiriting ')
+// let daraja = prompt('Darajasini kiriting')
+// let answer = son
+// for (let i = 0; i < daraja; i++) {
+//     answer = answer * son
+//     console.log(answer);
+    
+// }
+
